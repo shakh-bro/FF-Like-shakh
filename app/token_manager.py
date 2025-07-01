9b2d027bb06cbf2922be9bddddd4c45781ac3bfc
shakh-bro <shoh.2010.04@gmail.com>
2025-07-02 00:24:50 +0500

    ✅ get_headers() qayta tiklandi

--- app/token_manager.py
+++ app/token_manager.py
@@ -27,3 +27,14 @@ class TokenCache:
 
     def get_tokens(self, region):
         return self.cache.get(region.upper(), [])
+def get_headers(token: str):
+    return {
+        'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
+        'Connection': "Keep-Alive",
+        'Accept-Encoding': "gzip",
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Unity-Version": "2018.4.11f1",
+        "X-GA": "v1 1",
+        "ReleaseVersion": "OB49"
+    }
